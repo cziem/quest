47fe05378c4742c188dcd391f89b0531a56aaa9a
Fay Vor <phavorsparks@gmail.com>
2018-08-14 14:14:52 +0100

    Add Styling for login component

---
+++ src/components/HandleEntry.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/HandleEntry.css';
+import Login from './Login';
+
+export default class Registration extends Component {
+  render() {
+    return (
+      <div className="">
+        <Login />
+      </div>
+    )
+  }
+}
---
+++ src/components/Login.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitterSquare
+} from "react-icons/fa";
+import '../styles/Login.css';
+
+export default class Login extends Component {
+  state = {
+    error: undefined
+  };
+
+  handleLogin = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="container__main">
+        <h2 className="title">Login</h2>
+        <form onSubmit={this.handleLogin} className="login">
+          <div className="login__container">
+            <input type="text" name="username" required="required" />
+            <label for="username" className="login__details">
+              Username
+            </label>
+            <div className="bar" />
+          </div>
+          <div className="login__container">
+            <input type="password" name="password" required="required" />
+            <label for="password" className="login__details">
+              Password
+            </label>
+            <div className="bar" />
+          </div>
+          <label for="checkbox" className="remember login__container">
+            <input type="checkbox" />
+            Remember me
+          </label>
+          <div className="login__container button">
+            <button className="login-button btn">Let me in </button>
+          </div>
+          <footer className="login__container">
+            <p>or login with</p>
+            <div className="socials">
+              <FaFacebook />
+              <FaGithub />
+              <FaGoogle />
+              <FaInstagram />
+              <FaLinkedin />
+              <FaTwitterSquare />
+            </div>
+            <div className="extras">
+              <p>Forgot your password?</p>
+              <button className="btn register">Register</button>
+            </div>
+          </footer>
+        </form>
+      </div>
+    );
+  }
+}

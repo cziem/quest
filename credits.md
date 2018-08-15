abf645cb7829b27a7a13817076851a61e01acb14
Fay Vor <phavorsparks@gmail.com>
2018-08-15 16:20:57 +0100

    Styling Home page, This is quite some work

---
+++ credits.md
@@ -0,0 +1,7 @@
+# Credits
+
+## Photos
+
+- Photo by Michael Dam on Unsplash
+- Photo by Hadis Safari on Unsplash
+- Photo by Artiom Vallat on Unsplash
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,37 @@
 import React from 'react';
 import { IoIosSend } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 const Home = () => (
-  <div style={styles.body}>
-    <header>
+  <div className="main__wrap" >
+    <header className="header__home">
       <h2>quest</h2>
       <button>get started</button>
     </header>
 
     <main>
-      <h1>Express &bull; Inspire &bull; Share</h1>
+      <aside className="left">
+        <h1>Express &bull; Inspire &bull; Share</h1>
 
-      <p className="info">
-        Quest enhances your social health by taking away the worries of keeping up with every trend on every social platform.
-      </p>
+        <p className="info">
+          Quest enhances your social health by taking away the worries of keeping up with every trend on every social platform.
+        </p>
 
-      <p className="description">
-        One post, one message, one compose...
-        <span> multiple share <IoIosSend /></span>
-      </p>
+        <p className="description">
+          One post, one message, one compose...
+          <span> multiple share <IoIosSend /></span>
+        </p>
 
-      <section className="action">
-        <Link to='/login'>Let's Go</Link>
-      </section>
+        <section className="action">
+          <Link to='/login'>Let's Go</Link>
+        </section>
+      </aside>
+
+      <aside className="right overlay"></aside>
     </main>
 
-    <aside className="overlay"></aside>
   </div>
 )
 
 export default Home
-
-const styles = {
-  body: {
-    background: 'white',
-    width: '100%',
-    height: '100%'
-  }
-}

c83cedb04d212054911f5960af54fbf46758b65a
Fay Vor <phavorsparks@gmail.com>
2018-08-15 01:42:00 +0100

    Finished with Registration page

--- src/components/HandleEntry.js
+++ src/components/HandleEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/HandleEntry.css';
 import Login from './Login';
+import Registration from './Registration';
 
-export default class Registration extends Component {
+export default class HandleEntry extends Component {
   render() {
     return (
       <div className="">
-        <Login />
+        {/*<Login />*/}
+        <Registration />
       </div>
     )
   }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,10 +1,79 @@
 import React, { Component } from 'react';
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitterSquare,
+  FaChevronRight
+} from "react-icons/fa";
 
 export default class Registration extends Component {
+  handleRegister = (e) => {
+    e.preventDefault()
+  }
   render() {
     return (
-      <div className="">
-        <h1>Registration</h1>
+      <div className="container__main">
+        <h2 className="title">Registration</h2>
+        <form onSubmit={this.handleRegister}>
+          <div className="login__container">
+            <input
+              type="text"
+              name="name"
+              required="required"
+              placeholder="Name"
+            />
+            <div className="bar" />
+          </div>
+          <div className="login__container">
+            <input
+              type="text"
+              name="username"
+              required="required"
+              placeholder="Username"
+            />
+            <div className="bar" />
+          </div>
+          <div className="login__container">
+            <input
+              type="email"
+              name="email"
+              required="required"
+              placeholder="email"
+            />
+            <div className="bar" />
+          </div>
+          <div className="login__container">
+            <input
+              type="password"
+              name="password"
+              required="required"
+              placeholder="Password"
+            />
+            <div className="bar" />
+          </div>
+          <div className="login__container button">
+            <button className="login-button btn">
+              Register me <FaChevronRight className="arrow-right" />
+            </button>
+          </div>
+          <footer className="login__container-footer">
+            <div className="socials">
+              <p>or register with</p>
+              <div className="iconholder">
+                <FaFacebook className="social-icons" />
+                <FaGithub className="social-icons" />
+                <FaGoogle className="social-icons" />
+                <FaInstagram className="social-icons" />
+                <FaLinkedin className="social-icons" />
+                <FaTwitterSquare className="social-icons" />
+              </div>
+            </div>
+          </footer>
+        </form>
+        <h4 className="quest">Quest&copy;</h4>
       </div>
     )
   }

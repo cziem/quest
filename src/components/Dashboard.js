bd110264c6437ea8c72ca56b9de06f12d2932187
Fay Vor <phavorsparks@gmail.com>
2018-08-18 23:22:45 +0100

    Add Dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+        <h1>This is coming from the Dashboard</h1>
+      </div>
+    );
+  }
+};

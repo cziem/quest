897f9258ba743323c304677cacdbd4b941e0d07d
Fay Vor <phavorsparks@gmail.com>
2018-08-23 22:58:32 +0100

    update userprofile component

--- src/components/userProfiler/UserProfile.js
+++ src/components/userProfiler/UserProfile.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { FaPencilAlt, FaUser } from 'react-icons/fa';
 
 const UserProfile = (props) => (
   // This component contains the user's profile image, username and location
   <div style={style.profilerBox}>
+    <div style={style.editIcon}>
+      <FaPencilAlt />
+    </div>
     <div style={style.img__wrap}>
-      <p>This is a paragraph</p>
+      {
+        props.profileImage ? (<img src="" alt="{props.username}'s profile image" style={style.userImage} />) :
+        (<FaUser style={style.defaultImage}/>)
+      }
     </div>
     <footer>
-      {props.username && <p>{props.username}</p>}
+      {props.username ? (<p>{props.username}</p>) : (<p>quester</p>)}
     </footer>
   </div>
 )
@@ -16,10 +23,26 @@ export default UserProfile
 
 const style = {
   profilerBox: {
-    maxHeight: '10rem',
-    background: 'linearGradient(260deg, #191A42, #fcfcfc)'
+    background: 'linearGradient(260deg, #191A42, #fcfcfc)',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxHeight: '10rem'
   },
   img__wrap: {
+    height: '100%',
     width: '100%'
+  },
+  userImage: {
+    width: '100%'
+  },
+  defaultImage: {
+    maxWidth: '100%',
+    fontSize: '5rem',
+    textAlign: 'center'
+  },
+  editIcon: {
+    textAlign: 'center'
   }
 }

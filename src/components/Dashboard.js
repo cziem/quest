f04845acf8e7761d5502381aeff27157bffd2732
Fay Vor <phavorsparks@gmail.com>
2018-09-05 14:33:11 +0100

    Update dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,77 @@
 import React, { Component } from "react";
-import ActivityPane from "./ActivityPane";
+// import ActivityPane from "./ActivityPane";
 
 export default class Dashboard extends Component {
+  state = {
+    message: "",
+    error: ""
+  };
+
+  handleChange = e => {
+    let { message } = this.state;
+
+    const messageValue = e.target.value;
+
+    message = messageValue;
+
+    this.setState(() => ({ message }));
+  };
+
+  handleMessage = e => {
+    e.preventDefault();
+
+    let { message } = this.state;
+
+    if (message === "") {
+      this.setState(() => ({
+        error: "Message cannot be left empty"
+      }));
+    } else {
+      fetch("http://localhost:3001/server", {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+          Accept: "Application/json"
+        },
+        body: JSON.stringify({
+          message
+        })
+      })
+        .then(response => { return response.json() } )
+        .then((res) => {
+
+          if (res.message !== 'empty') {
+            console.log(res.message)
+            this.setState(() => ({
+              message: ""
+            }));
+            // {
+            //   <p>{}</p>;
+            // }
+          }
+        })
+        .catch(err => console.log("An error occurred", err));
+    }
+  };
+
   render() {
+    let { message } = this.state;
+
     return (
       <div style={style.dashboard}>
         {/* Activity Panel */}
-        <ActivityPane />
+        {/* <ActivityPane /> */}
+        <form onSubmit={this.handleMessage}>
+          <input
+            type="text"
+            name="message"
+            placeholder="Write a message..."
+            onChange={this.handleChange}
+            value={message}
+          />
+          <button>Send Now</button>
+          {<p>{this.state.error}</p>}
+        </form>
       </div>
     );
   }
@@ -14,8 +79,9 @@ export default class Dashboard extends Component {
 
 const style = {
   dashboard: {
-    marginTop: '-1.2rem', // FIXME
-    height: '100vh',
-    width: '100%'
+    // marginTop: '-1.2rem', // FIXME
+    padding: "3rem", // FIXME
+    height: "100vh",
+    width: "100%"
   }
-}
+};

0d3d620bdf942cbc7c54dbdd8ec0430a0a0093c1
Fay Vor <phavorsparks@gmail.com>
2018-09-06 16:37:08 +0100

    Add Interactive component

---
+++ src/components/Interactive.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Interactive extends Component {
+  render() {
+    return (
+      <div className="app__interact">
+        <div className="welcome_msg">
+          <h1>Hi there! Welcome to Quest</h1>
+          <p>what would you like to do...</p>
+        </div>
+      </div>
+    )
+  }
+}

2abeb2857d95db0c0168a291d3065c52cd502c08
Fay Vor <phavorsparks@gmail.com>
2018-09-07 13:11:51 +0100

    get userdetails

--- src/components/ActivityPane.js
+++ src/components/ActivityPane.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import UserProfile from './userProfiler/UserProfile';
+import Axios from 'axios';
 
 export default class ActivityPane extends Component {
+  state = {
+    profileDetails: {
+      name: '',
+      photo: '',
+      password: '',
+      username: '',
+      email: ''
+    }
+  }
+
+  componentWillMount = () => {
+    Axios({
+      method: 'GET',
+      url: 'http://localhost:3001/server'
+    })
+      .then(res => console.dir(res.data))
+      .catch(err => console.log('could not GET the data', err))
+  }
+
   render () {
     return (
       <div className='paneWrapper'>

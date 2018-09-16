11a2f37bb5f5915b6ac7a61bcaec3f549838a616
Fay Vor <phavorsparks@gmail.com>
2018-09-16 22:57:45 +0100

    updated schema with reqiured fields

--- models/schema.js
+++ models/schema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const AppSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, trim: true, lowercase: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   date: { type: Date, default: Date.now }

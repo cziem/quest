df8852eb99b5596eed328c141293cb66004018e8
Fay Vor <phavorsparks@gmail.com>
2018-09-19 23:11:55 +0100

    authorization

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,11 +4,19 @@ const mongoose = require('mongoose'),
      _ = require('lodash');
 
 const { User, validateUser } = require('../models/user')
+const auth_mid = require('../middleware/middleware_auth')
 
 const app = express();
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+// Authorization Process
+router.get('/me', auth_mid, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password') // we should change the password exclusion much later
+  res.send(user)
+ })
+
+/* 
+router.get('/me', auth_mid, async (req, res) => {
   const user = await User
     .find()
     .select([
@@ -18,5 +26,5 @@ router.get('/', async (req, res) => {
     ])
   res.json(user)
 })
-
+ */
 module.exports = router;

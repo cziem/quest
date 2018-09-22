25ee1a313e9470d8b9f0c4264cf0fe64cc3268dd
Fay Vor <phavorsparks@gmail.com>
2018-09-22 22:25:03 +0100

    Logout removes token from localStorage and redirects to login

--- src/components/ActivityPane.js
+++ src/components/ActivityPane.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import UserProfile from './userProfiler/UserProfile';
 import ListItems from './sideBarLists/ListItems';
 import Button from './Button';
@@ -12,10 +13,26 @@ const ActivityPane = props => (
       toggle={ props.toggleState }
     />
     <div className='account_activities'>
-      <Button cName="btn-account" name="Account"/>
-      <Button cName="btn-account" name="Logout" /> { /* destroy jwt here */}
+      <button
+        className="btn-account"
+        // onClick={handleAccount}
+      >
+        Account
+      </button>
+      <button
+        className="btn-account"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
     </div>
   </div>
 )
+
+const handleLogout = () => {
+  // localStorage.removeItem('token')
+  localStorage.clear()
+  return window.location.href = `http://localhost:3000/login`
+}
 
 export default ActivityPane
